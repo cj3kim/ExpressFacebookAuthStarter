c674d00407a4f6d35c87937fd8463ebd37eef246
Chris Kim <cj3kim@gmail.com>
2015-05-27 13:15:49 -0700

    We create a migration for the user table. Migrations are essentially table construction plans to be followed by the database when executed with knex migrate:latest

---
+++ config/db/migrations/20150527131010_create_users_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function (table) {
+    t.increments().primary();
+    t.string('full_name');
+    t.string('email');
+    t.string('fb_access_token');
+
+    t.string('created_at').notNullable().defaultTo(knex.raw('now()'));
+    t.string('updated_at').notNullable().defaultTo(knex.raw('now()'));
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
